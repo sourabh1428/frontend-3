e646424a17814d18db966a5067ce6ea5f7e31034
sourabh1428 <sppathak1428@gmail.com>
2023-07-29 19:16:26 +0530

    Your commit message here

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+        $('#backToTopBtn').fadeIn();
+      } else {
+        $('#backToTopBtn').fadeOut();
+      }
+    });
+  
+    $('#backToTopBtn').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
+      return false;
+    });
+  });
+  
